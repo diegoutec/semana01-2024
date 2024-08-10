caa2bb86c12893b9ab20d9511245f22b66193580
diego.zuppardi <diego.zuppardi@estudiantes.utec.edu.uy>
2024-08-10 09:34:56 -0300

    commit 002

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    // Solicitar al usuario que ingrese una cadena de caracteres
+    // Solicitar el ingreso una cadena de caracteres
     println("Por favor, ingresa una cadena de caracteres:")
-    val input = readlnOrNull() ?: "" // Leer la entrada del usuario
+    val entrada = readlnOrNull() ?: "" // Leer la entrada del usuario
 
-    // Inicializar el contador de vocales
+    // Contador de vocales
     var contadorVocales = 0
 
     // Definir las vocales (tanto mayúsculas como minúsculas)
     val vocales = "aeiouAEIOU"
 
     // Iterar a través de cada caracter en la cadena ingresada
-    for (caracter in input) {
+    for (caracter in entrada) {
         // Si el caracter está en la lista de vocales, incrementar el contador
         if (caracter in vocales) {
             contadorVocales++
@@ -20,5 +18,5 @@ fun main() {
     }
 
     // Mostrar el número total de vocales encontradas
-    println("El número total de vocales en la cadena es: $contadorVocales")
+    println("El número total de vocales ingresadas son: $contadorVocales")
 }
